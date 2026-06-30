a2b008eb9e7262e9655562774d82641ec9112980
Drag93 <faithyg93@gmail.com>
2026-06-30 15:26:42 +0900

    header, footer 기본세팅

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className="header">
+        <div className="nav-wrap">
+          <div className="gnb-left">
+            <ul>
+              <li>
+                <Link to={"/store"}>스토어</Link>
+              </li>
+              <li>
+                <Link to={"/community"}>게시판</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="gnb-logo">
+            <Link to={"/"}>
+              <h1>logo</h1>
+            </Link>
+          </div>
+          <div className="gnb-right">
+            <ul>
+              <li>
+                <Link to={"/login"}>로그인</Link>
+              </li>
+              <li>
+                <Link to={"/join"}>회원가입</Link>
+              </li>
+              <li>
+                <Link to={"/cart"}>장바구니</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
