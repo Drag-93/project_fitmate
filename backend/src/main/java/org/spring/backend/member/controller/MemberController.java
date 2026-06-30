463b548ed3bb88ef545b372b6f5fc7e228ea7d14
kimwoosong <kaope0439@gmail.com>
2026-06-30 17:24:38 +0900

    0630시큐리티기능 추가중, memberController join기능 추가

--- backend/src/main/java/org/spring/backend/member/controller/MemberController.java
+++ backend/src/main/java/org/spring/backend/member/controller/MemberController.java
@@ -19,20 +19,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/join")
-    public ResponseEntity<?> join(@Valid @RequestBody MemberDto memberDto,
-                                  BindingResult bindingResult){
-        // 유효성 검사 실패 시
-        if (bindingResult.hasErrors()) {
-            // 에러 메시지들을 맵이나 리스트로 모아서 반환
-            Map<String, String> errorMap = new HashMap<>();
-            bindingResult.getFieldErrors().forEach(error ->
-                    errorMap.put(error.getField(), error.getDefaultMessage())
-            );
-
-            // 400 Bad Request 상태코드와 함께 에러 데이터 반환
-            return ResponseEntity.badRequest().body(errorMap);
-        }
-
+    public ResponseEntity<?> join(MemberDto memberDto){
+        
         // 회원가입 비즈니스 로직 실행
         memberService.insertMember(memberDto);
 
