1456f3232950374db97254d8c880b611dea96c8b
wngml2363 <wngml2363@gamil.com>
2026-07-02 17:21:26 +0900

    ProductController작성

--- backend/src/main/java/org/spring/backend/store/product/controller/ProductController.java
+++ backend/src/main/java/org/spring/backend/store/product/controller/ProductController.java
@@ -1,5 +1,23 @@
 package org.spring.backend.store.product.controller;
 
+import org.spring.backend.store.product.service.ProductService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+  private final ProductService productService;
+
+  @GetMapping("path")
+  public String getMethodName(@RequestParam String param) {
+      return new String();
+  }
   
 }
