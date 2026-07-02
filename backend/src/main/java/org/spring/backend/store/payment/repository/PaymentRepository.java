a43803c03fe633b48b11ba4277a606cf961a479b
wngml2363 <wngml2363@gamil.com>
2026-07-02 17:06:13 +0900

    PaymentRepository 수정

--- backend/src/main/java/org/spring/backend/store/payment/repository/PaymentRepository.java
+++ backend/src/main/java/org/spring/backend/store/payment/repository/PaymentRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.spring.backend.store.payment.entity.PaymentEntity;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
 
