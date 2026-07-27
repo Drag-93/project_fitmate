8970f8522fe1b89a03552af56ea039f9471c6a8d
lhstk114 <lhstk114@gmail.com>
2026-07-27 11:20:16 +0900

    yml path

--- backend/src/main/java/org/spring/backend/community/repository/CommunityRepository.java
+++ backend/src/main/java/org/spring/backend/community/repository/CommunityRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import io.lettuce.core.dynamic.annotation.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
